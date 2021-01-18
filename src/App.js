6f0d7c1ee6ba2bbb34990be54aafe83bbba5578f
abc <abc@example.com>
2021-01-18 16:34:47 -0500

    header implementation / issue with options flex

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 function App() {
   return (
+    <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
     </Switch>
+    </div>
   );
 }
 
