f32bb15cc44958566855ec917a840f1c2bbe22d5
abc <abc@example.com>
2021-01-19 20:23:26 -0500

    sign-in and form components, shrink functionality still an issue...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in/sign-in-sign-up.component.jsx';
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
+      <Route path='/signin' component={SignInAndSignUpPage}/>
     </Switch>
     </div>
   );
---
+++ src/pages/sign-in/sign-in-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sign-in-sign-up.styles.scss';
+
+import SignIn from '../../components/sign-in/signIn.component';
+
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUpPage;
