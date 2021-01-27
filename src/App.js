f4df8337d7b6a01b6f7423c32a73e1cad111ac59
abc <abc@example.com>
2021-01-27 16:20:25 -0500

    implemented firebase utils, ability to store authenticated users into firestore

--- src/App.js
+++ src/App.js
@@ -32,8 +32,10 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          })
+          });  
+          console.log(this.state)
         });
+       
       }
      else {
        this.setState({ currentUser:userAuth });
