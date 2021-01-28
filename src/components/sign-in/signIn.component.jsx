1bbb0f20b72f61a4ffc390ae822f93c57683c7b6
abc <abc@example.com>
2021-01-28 12:23:18 -0500

    displayName not populating

--- src/components/sign-in/signIn.component.jsx
+++ src/components/sign-in/signIn.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './signIn.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../button/button.component.jsx'
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props){
@@ -14,10 +14,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state;
+
+        try {
+            auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''});
+        } catch (error){
+            console.log(error);
+        }
     }
 
     handleChange = event => {
@@ -39,7 +46,7 @@ class SignIn extends React.Component {
                     
                     <div className='buttons'>
                     <CustomButton name='submit' type='submit'>Sign in</CustomButton>
-                    <CustomButton name='submit' onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                    <CustomButton name='submit' type='button' onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
