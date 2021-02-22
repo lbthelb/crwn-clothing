6da0200e5cdd678d9c7065b80eab7e46136ef6e7
abc <abc@example.com>
2021-02-22 15:11:36 -0500

    updated app to redirect to home if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux'; 
 
 import './header.styles.scss'; 
 
@@ -20,4 +21,8 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
   <App />
   </BrowserRouter>,
- 
+  </Provider>,
   document.getElementById('root')
 );
 
