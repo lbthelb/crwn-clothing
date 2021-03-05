fa975f100da3324e95067f7c56e3c0de36d8d291
abc <abc@example.com>
2021-03-05 13:19:24 -0500

    resolved cart dropdown issue

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart.styles.scss';
 
-const Cart = () => (
-    <div className='cart-dropdown' >
-        <div className='cart-items' />
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+const Cart = ({ cartItems }) => (
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
-)
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(Cart);
