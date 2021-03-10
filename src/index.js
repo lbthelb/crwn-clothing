de186f9230ff134434d13ec949adc4c12891a9d1
abc <laurenabennett@gmail.com>
2021-03-09 21:21:26 -0500

    created selectors for shop and directory objects

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
+  <PersistGate persistor={persistor}>
   <App />
+  </PersistGate>
   </BrowserRouter>,
   </Provider>,
   document.getElementById('root')
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,32 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollections } from '../../redux/shop/shop.selector';
 
-
-import SHOP_DATA from './shop-data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-
-
-    render(){
-        const {collections} = this.state;
-        return (
+const ShopPage = ({ collections }) => (
         <div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
+            {collections.map(({id, ...otherCollectionProps}) => (
                     <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))
-            }
+                ))}
+            </div>
+    );
 
-        </div>);
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-export default ShopPage;
+
+export default connect(mapStateToProps)(ShopPage);
