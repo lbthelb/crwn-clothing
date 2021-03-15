455a833965a65aa6375a6f7cf77fc3959a0ffcfc
abc <laurenabennett@gmail.com>
2021-03-15 13:56:23 -0400

    normalized collections preview and populated collections page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,11 +6,19 @@ import { selectCollection } from '../../redux/shop/shop.selector';
 
 import './collection.styles.scss';
 
-const CollectionPage = () => (
-    <div className='category'>
-        <h2>Collection Page</h2>
-    </div>
-);
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (
+      <div className='collection-page'>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+          {items.map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    );
+  };
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,7 +8,7 @@ import CollectionPage from '../collection/collection.component';
 const ShopPage = ({ match }) => (
         <div className='shop-page'>
           <Route exact path={`${match.path}`} component={CollectionOverview} />
-          <Route path={`${match.path}/:categoryId`} component={CollectionPage}/>
+          <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
             </div>
     );
 
